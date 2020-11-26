d656b698def076b8eacf74e1906e381abfa98f96
IVAN QUIAB <al047218@uacam.mx>
2020-11-26 17:53:03 -0600

    Version con 2 pruebas en cada funcion de la calculadora

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -3,79 +3,38 @@ import java.util.Scanner;
 public class Calculadora {
     int numero1 = 0;
     int numero2 = 0;
-    //Creacion del scanner para leer los numeros.
-    Scanner sc = new Scanner(System.in);
 
     public void Calculadora(){
-        //preguntar que operacion desea hacer
-        System.out.println("¿Qué operación desea hacer?");
-        System.out.println("Suma: 1\nResta: 2\nMutiplicación: 3\nDivisión: 4\nPotencia: 5");
-        int operacion = sc.nextInt();
-
-        //Segun el numero introducido es la operacion que se realizara
-        switch (operacion)
-        {
-            case 1:
-                System.out.println("Sumando");
-                this.suma();
-                break;
-            case 2:
-                System.out.println("Restando");
-                this.resta();
-                break;
-            case 3:
-                System.out.println("Multiplicando");
-                this.multiplicación();
-                break;
-            case 4:
-                System.out.println("Dividiendo");
-                this.potencia();
-                break;
-            case 5:
-                System.out.println("Potencia");
-                break;
-            default:
-                System.out.println("No es una operación correcta");
-        }
+        System.out.println("Calculadora funcionando");
     }
 
-    public int suma(){
-        System.out.println("Ingresa el primer numero");
-        numero1= sc.nextInt();
-        System.out.println("Ingresa el segundo numero");
-        numero2= sc.nextInt();
+    public int suma(int a,int b){
+        numero1 = a;
+        numero2 = b;
         return numero1+numero2;
     }
 
-    public int resta(){
-        System.out.println("Ingresa el minuendo");
-        numero1= sc.nextInt();
-        System.out.println("Ingresa el sustraendo");
-        numero2= sc.nextInt();
-        return numero1-2;
+    public int resta(int a,int b){
+        numero1 = a;
+        numero2 = b;
+        return numero1-numero2;
     }
 
-    public int multiplicación(){
-        System.out.println("Ingresa el primer factor");
-        numero1= sc.nextInt();
-        System.out.println("Ingresa el segundo factor");
-        numero2= sc.nextInt();
+    public int multiplicación(int a,int b){
+        numero1 = a;
+        numero2 = b;
         return numero1*numero2;
     }
 
-    public int division() {
-        System.out.println("Ingresa el dividendo");
-        numero1= sc.nextInt();
-        System.out.println("Ingresa el divisor");
-        numero2= sc.nextInt();
+    public int division(int a,int b) {
+        numero1 = a;
+        numero2 = b;
         return numero1/numero2;
     }
 
-    public int potencia(){
-        System.out.println("Ingresa la base");
-        numero1= sc.nextInt();
-        System.out.println("Ingresa el exponente");
-        numero2= sc.nextInt();
+    public int potencia(int a,int b){
+        numero1 = a;
+        numero2 = b;
         return (int) Math.pow(numero1,numero2);
     }
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,3 +1,80 @@
+//Librerias que se utilizan para el test
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class CalculadoraTest {
+
+    Calculadora calc;
+
+    //Se ejecutara antes del cada Test
+    @Before
+    public void iniciador(){
+        //Creacion del objeto
+        calc = new Calculadora();
+    }
+
+    @Test
+    public void suma1(){
+        int resultado = calc.suma(10,20);
+        int exp = 30;
+        assertEquals(exp,resultado);
+    }
+
+    @Test
+    public void suma2(){
+        int resultado = calc.suma(50,125);
+        int exp = 175;
+        assertEquals(exp,resultado);
+    }
+    @Test
+    public void resta1(){
+        int resultado = calc.resta(10,20);
+        int exp = -10;
+        assertEquals(exp,resultado);
+    }
+    @Test
+    public void resta2(){
+        int resultado = calc.resta(125,20);
+        int exp = 105;
+        assertEquals(exp,resultado);
+    }
+    @Test
+    public void multiplicacion1(){
+        int resultado = calc.multiplicación(10,20);
+        int exp = 200;
+        assertEquals(exp,resultado);
+    }
+    @Test
+    public void multiplicacion2(){
+    int resultado = calc.multiplicación(5,20);
+    int exp = 100;
+    assertEquals(exp,resultado);
+
+    }
+    @Test
+    public void division1(){
+        int resultado = calc.division(10,2);
+        int exp = 5;
+        assertEquals(exp,resultado);
+    }
+    @Test
+    public void division2(){
+        int resultado = calc.division(100,20);
+        int exp = 5;
+        assertEquals(exp,resultado);
+    }
+    @Test
+    public void potencia1(){
+        int resultado = calc.potencia(10,2);
+        int exp = 100;
+        assertEquals(exp,resultado);
+    }
+    @Test
+    public void potencia2(){
+        int resultado = calc.potencia(2,3);
+        int exp = 8;
+        assertEquals(exp,resultado);
+    }
 
 }
